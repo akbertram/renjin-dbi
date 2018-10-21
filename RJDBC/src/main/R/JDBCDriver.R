0dd4578788a8b72001072e1bc3aa8bda3e7d1e4d
Alex Bertram <alex@bedatadriven.com>
2018-10-21 12:40:25 +0200

    Corrected problems in JDBCDriver class

--- RJDBC/src/main/R/JDBCDriver.R
+++ RJDBC/src/main/R/JDBCDriver.R
@@ -6,8 +6,8 @@
 setClass("JDBCDriver", representation("DBIDriver", identifier.quote="character", ptr="externalptr"))
 
 JDBC <- function(driverClass='', classPath='', identifier.quote=NA) {
-  driver <- import(driverClass)
-  new("JDBCDriver", identifier.quote=as.character(identifier.quote), ptr=driver)
+  driver <- do.call('import', list(as.name(driverClass)))
+  new("JDBCDriver", identifier.quote=as.character(identifier.quote), ptr=driver$new())
 }
 
 setMethod("dbListConnections", "JDBCDriver", def=function(drv, ...) {
@@ -25,11 +25,11 @@ setMethod("dbGetInfo", "JDBCDriver", def=function(dbObj, ...)
 setMethod("dbUnloadDriver", "JDBCDriver", def=function(drv, ...) FALSE)
 
 setMethod("dbConnect", "JDBCDriver", def=function(drv, url, user='', password='', ...) {
-	if (getOption("dbi.debug", F)) message("II: Connecting to ",url," with user ", username, " and a non-printed password.")
+	if (getOption("dbi.debug", F)) message("II: Connecting to ",url," with user ", user, " and a non-printed password.")
 	prop <- import(java.util.Properties)$new()
-	prop$setProperty("user", username)
+	prop$setProperty("user", user)
 	prop$setProperty("password", password)
-	jconn <- drv$ptr$connect(url, prop)
+	jconn <- drv@ptr$connect(url, prop)
   new("JDBCConnection", ptr=jconn, identifier.quote=drv@identifier.quote)},
           valueClass="JDBCConnection")
 
